d98441ef9ab246802671005b429a471e8fa3e67e
Pranjal Jhariya <pranjaljhariya11@gmail.com>
2026-07-03 19:24:03 +0530

    Update countdights.py

--- countdights.py
+++ countdights.py
@@ -1,9 +1,14 @@
-num=int(input("Enter a number :"))
-if num==0:
-   count=1
-else:
-     count=0
-     while num>0:
-        count+=1
-        num=num//10
-print("Digits=",count)
+def count_digits(num):
+    if num == 0:
+        return 1
+    count = 0
+    num = abs(num)
+    while num > 0:
+        count += 1
+        num = num // 10
+    return count
+try:
+    num = int(input("Enter a number: "))
+    print(f"Number of digits = {count_digits(num)}")
+except ValueError:
+    print("Please enter a valid integer!")
