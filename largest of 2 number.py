20445955da2719e17b562798441ba1bb92ed9296
Pranjal Jhariya <pranjaljhariya11@gmail.com>
2026-07-03 20:40:42 +0530

    Update largest of 2 number.py

--- largest of 2 number.py
+++ largest of 2 number.py
@@ -1,6 +1,7 @@
-a=int(input("Enter first number:"))
-b=int(input("Enter second number:"))
-if a>b:
-    print("Largest=",a)
-else:
-    print("Largest=",b)
+try:
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    largest = max(a, b)
+    print(f"Largest number is: {largest}")
+except ValueError:
+    print("Please enter valid numbers!")
