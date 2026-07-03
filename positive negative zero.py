b88fc4e05b25194d5d74d1e7082d26e143c817c9
Pranjal Jhariya <pranjaljhariya11@gmail.com>
2026-07-03 20:35:46 +0530

    Update and rename positivenegativezero.py to positive negative zero.py

---
+++ positive negative zero.py
@@ -0,0 +1,15 @@
+def check_number(num):
+    if num > 0:
+        return "Positive"
+    elif num < 0:
+        return "Negative"
+    else:
+        return "Zero"
+
+# Main Program
+try:
+    num = float(input("Enter a number: "))
+    result = check_number(num)
+    print(f"The number is: {result}")
+except ValueError:
+    print("Please enter a valid number!")
--- positivenegativezero.py
+++
@@ -1,8 +0,0 @@
-num=int(input("Enter a number:"))
-if num>0:
-    print("positive")
-elif num<0:
-    print("negative")
-else:
-    print("zero")
-    
