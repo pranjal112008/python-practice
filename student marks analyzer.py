fc43a56d53f8935a6d2acbd0fbc28a75a095b2ae
Pranjal Jhariya <pranjaljhariya11@gmail.com>
2026-07-03 20:42:23 +0530

    Update student marks analyzer.py

--- student marks analyzer.py
+++ student marks analyzer.py
@@ -5,18 +5,13 @@ def analyze_marks(marks):
     lowest = min(marks)
     passed = sum(1 for m in marks if m >= 40)
     failed = len(marks) - passed
-    
     return total, average, highest, lowest, passed, failed
-
-# Main Program
 n = int(input("How many students? "))
 marks = []
 for i in range(n):
     m = int(input(f"Enter marks of student {i+1}: "))
     marks.append(m)
-
 total, average, highest, lowest, passed, failed = analyze_marks(marks)
-
 print("\n" + "="*40)
 print("          MARKS ANALYZER REPORT")
 print("="*40)
