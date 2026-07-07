6bf3e68326be438c0eb66610fdd6eedad15e9ee6
Pranjal Jhariya <pranjaljhariya11@gmail.com>
2026-07-07 21:39:23 +0530

    Add files via upload

---
+++ valid anagram.py
@@ -0,0 +1,11 @@
+def isAnagram(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    count = [0] * 26
+    for char in s:
+        count[ord(char) - ord('a')] += 1
+    for char in t:
+        count[ord(char) - ord('a')] -= 1
+    return all(x == 0 for x in count)
+result = isAnagram(input("Enter first string: ").capitalize(), input("Enter second string: ").capitalize())
+print(result)
