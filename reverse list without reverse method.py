436abd46b7e89026355733a1d82b516f2321e5a4
Pranjal Jhariya <pranjaljhariya11@gmail.com>
2026-07-13 21:11:32 +0530

    Add files via upload

---
+++ reverse list without reverse method.py
@@ -0,0 +1,9 @@
+import numbers
+def reverse_list(lst):
+    reversed_lst = []
+    for i in range(len(lst)-1, -1, -1):
+        reversed_lst.append(lst[i])
+    return reversed_lst
+numbers = list(map(int, input("Enter a list of numbers separated by spaces: ").split()))
+print("Original List:", numbers)
+print("Reversed List:", reverse_list(numbers))
